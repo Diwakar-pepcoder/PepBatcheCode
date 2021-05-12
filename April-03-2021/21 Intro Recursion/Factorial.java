038629d13c2dfd54be86a63cba310280666460b8
diwakargupta815@gmail.com <Diwakar>
2021-05-12 23:11:20 +0530

    april Intro to recursion

---
+++ April-03-2021/21 Intro Recursion/Factorial.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int facn = factorial(n);
+    System.out.println(facn);
+  }
+
+  public static int factorial(int n) {
+    if (n == 0)return 1;
+
+    int fa = factorial(n - 1);
+    int mya = n * fa;
+    return mya;
+  }
+
+}
