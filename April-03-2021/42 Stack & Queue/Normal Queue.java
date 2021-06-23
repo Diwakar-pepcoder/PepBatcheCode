264e82d813935e9441bc5e71a98d4f71b8d2b941
Diwakar Gupta <diwakargupta815@gmail.com>
2021-06-23 22:17:07 +0530

    dsa

---
+++ April-03-2021/42 Stack & Queue/Normal Queue.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class CustomQueue {
+    int[] data;
+    int front;
+    int size;
+
+    public CustomQueue(int cap) {
+      data = new int[cap];
+      front = 0;
+      size = 0;
+    }
+
+    int size() {
+        return size;
+    }
+
+    void display() {
+        for(int i=0;i<size;i++){
+            int index = front+i;
+            if(index >= data.length){
+                index = index - data.length;
+            }
+            System.out.print(data[index]+" ");
+        }
+        System.out.println();
+    }
+
+    void add(int val) {
+        if(size == data.length){
+            System.out.println("Queue overflow");
+        }else{
+            int index = front + size;
+            if(index >= data.length){
+                index = index - data.length;
+            }
+            data[index] = val;
+            size++;
+        }
+    }
+
+    int remove() {
+        if(size == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }else{
+            int val = data[front];
+            front++;
+            if(front == data.length)front = 0;
+            size--;
+            return val;
+        }
+    }
+
+    int peek() {
+       if(size == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }else{
+            return data[front];
+        }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    CustomQueue qu = new CustomQueue(n);
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("add")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if(str.startsWith("remove")){
+        int val = qu.remove();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("peek")){
+        int val = qu.peek();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("size")){
+        System.out.println(qu.size());
+      } else if(str.startsWith("display")){
+        qu.display();
+      }
+      str = br.readLine();
+    }
+  }
+}
