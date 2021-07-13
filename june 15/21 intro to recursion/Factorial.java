e8f09816d1cbfc54b93ec6782d5c30d340dfb4ce
Diwakar Gupta <diwakargupta815@gmail.com>
2021-07-13 22:04:51 +0530

    eqw

---
+++ june 15/21 intro to recursion/Factorial.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int fac = factorial(n);
+        System.out.println(fac);
+    }
+
+    public static int factorial(int n){
+        if(n==0)return 1;
+        int fnm1 = factorial(n-1);
+        int fn = n * fnm1;
+        return fn;
+    }
+
+}
